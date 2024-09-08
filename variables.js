6bcea26bc50cfed8c9831279db7a6812c57342a1
JustChad1 <ramosrichard176@gmail.com>
2024-09-08 21:13:20 +0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'Ramos, Richard Michael V.';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+
+// Create a variable named gender and store your gender
+let gender = 'Male';
+
+// Create a variable named TwitterHandle and store your Twitter Handle
+let TwitterHandle = '@Richard66118818';
+
+// Log all the variables to the console
+console.log('What is your name? ' name);
+console.log('What do you wanna become in your life? ' whatDoYouWannaBecomeInYourLife);
+console.log('Are you Male or Female? ' gender);
+console.log('Twitter Handle' TwitterHandle);
